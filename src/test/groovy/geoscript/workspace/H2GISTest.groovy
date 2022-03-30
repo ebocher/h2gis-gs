a239c9a1f0ddb3319ab6052b3daa6983790dd16b
ebocher <erwan.bocher@univ-ubs.fr>
2022-03-30 20:47:13 +0200

    Prepare new version with last geoscript

--- src/test/groovy/geoscript/workspace/H2GISTest.groovy
+++ src/test/groovy/geoscript/workspace/H2GISTest.groovy
@@ -6,7 +6,6 @@ import geoscript.layer.Layer
 import org.junit.jupiter.api.Test
 
 import static org.junit.jupiter.api.Assertions.assertEquals
-import static org.junit.jupiter.api.Assertions.assertEquals
 import static org.junit.jupiter.api.Assertions.assertNotNull
 
 class H2GISTest {
@@ -14,6 +13,7 @@ class H2GISTest {
     @Test void createFromPath() {
         H2GIS h2gis = new H2GIS("./target/mydb")
         assertEquals "H2GIS", h2gis.format
+        h2gis.getSql().execute("drop table if exists \"widgets\" ")
         Layer l = h2gis.create('widgets',[new Field("geom", "Point"), new Field("name", "String")])
         assertNotNull(l)
         l.add([new Point(1,1), "one"])
